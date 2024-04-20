04924045714e5d45180d5afd83a63472433314d4
alazarbeyeneazu <alazarbeyene99@gmail.com>
2024-04-20 22:14:47 +0300

    user persistence done

--- internal/storage/persistence/persistence.go
+++ internal/storage/persistence/persistence.go
@@ -1,4 +1,18 @@
 package persistence
 
+import (
+	"context"
+
+	"github.com/alazarbeyeneazu/gms-backend/internal/constants/model/dto"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type User interface {
+	SaveUser(ctx context.Context, user dto.User) error
+	UpdateUser(ctx context.Context, filter bson.M, update bson.M) error
+	DeleteUser(ctx context.Context, userID primitive.ObjectID) error
 }
+type Customer interface {
+}
+type PaymentRule interface{}
