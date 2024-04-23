bc8906f007303c3aa395b53a747a05f06abff922
alazarbeyeneazu <alazarbeyene99@gmail.com>
2024-04-23 10:58:38 +0300

    handler and route done for payment method

--- internal/constants/model/dto/dto.go
+++ internal/constants/model/dto/dto.go
@@ -1,6 +1,8 @@
 package dto
 
 import (
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -14,6 +16,13 @@ type Customer struct {
 	UserID primitive.ObjectID `json:"user_id" bson:"user_id" validate:"required"`
 }
 type PaymentRule struct {
-	ID   primitive.ObjectID `json:"id" bson:"_id"`
-	Type string             `json:"type" bson:"type"`
+	ID           primitive.ObjectID `json:"id" bson:"_id"`
+	Type         string             `json:"type" bson:"type" validate:"required"`
+	Payment      float64            `json:"payment" bson:"payment" validate:"required"`
+	DaysPerWeek  int                `json:"days_per_week" bson:"days_per_week" validate:"required"`
+	TimesPerDay  int                `json:"times_per_day" bson:"times_per_day" validate:"required"`
+	NumberOfDays int                `json:"number_of_days" bson:"number_of_days" validate:"required"`
+	CreatedAt    time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt    time.Time          `json:"updated_at" bson:"updated_at"`
+	DeletedAt    time.Time          `json:"deleted_at" bson:"deleted_at"`
 }
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -6,4 +6,11 @@ type User interface {
 	RegisterUser(c *gin.Context)
 	UpdateUser(c *gin.Context)
 	DeleteUser(c *gin.Context)
+	GetUsers(c *gin.Context)
+}
+type PaymentRule interface {
+	CreatePaymentRule(c *gin.Context)
+	UpdatepaymentRule(c *gin.Context)
+	GetPaymentRule(c *gin.Context)
+	DeletepaymentRule(c *gin.Context)
 }
