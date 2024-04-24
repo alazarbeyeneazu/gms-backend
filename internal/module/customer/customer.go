d01b594fb7d9e59dce604f175b8861a4ebd3405e
alazarbeyeneazu <alazarbeyene99@gmail.com>
2024-04-24 16:29:37 +0300

    handle customer persistence layer done

--- internal/module/customer/customer.go
+++ internal/module/customer/customer.go
@@ -1,31 +1,105 @@
 package customer
 
 import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/alazarbeyeneazu/gms-backend/internal/constants/errors"
+	"github.com/alazarbeyeneazu/gms-backend/internal/constants/model/dto"
+	"github.com/alazarbeyeneazu/gms-backend/internal/module"
 	"github.com/alazarbeyeneazu/gms-backend/internal/storage/persistence"
+	"github.com/go-playground/validator"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.uber.org/zap"
 )
 
 type customer struct {
-	log    *zap.Logger
-	userDB persistence.User
+	log       *zap.Logger
+	db        persistence.Customer
+	userDB    persistence.User
+	PaymentDB persistence.PaymentRule
 }
 
-// func Init(userdb persistence.User, log *zap.Logger) module.Customer {
-// 	return &customer{
-// 		log:    log,
-// 		userDB: userdb,
-// 	}
-// }
-// func (u *customer) RegisterUser(ctx context.Context, user dto.User) (dto.User, error) {
-// 	validate := validator.New()
-// 	if err := validate.Struct(&user); err != nil {
-// 		u.log.Error("validation error ", zap.Error(err))
-// 		err = errors.ErrInvalidUserInput.Wrap(err, err.Error())
-// 		return dto.User{}, err
-// 	}
-// 	user.ID = primitive.NewObjectID()
-// 	if err := u.userDB.SaveUser(ctx, user); err != nil {
-// 		return dto.User{}, err
-// 	}
-// 	return user, nil
-// }
+func Init(db persistence.Customer, paymentdb persistence.PaymentRule, userdb persistence.User, log *zap.Logger) module.Customer {
+	return &customer{
+		log:       log,
+		userDB:    userdb,
+		db:        db,
+		PaymentDB: paymentdb,
+	}
+}
+
+func (u *customer) RegisterUser(ctx context.Context, customer dto.RegisterCustomer) (dto.RegisterCustomer, error) {
+	validate := validator.New()
+	if err := validate.Struct(&customer); err != nil {
+		u.log.Error("validation error ", zap.Error(err))
+		err = errors.ErrInvalidUserInput.Wrap(err, err.Error())
+		return dto.RegisterCustomer{}, err
+	}
+	customer.ID = primitive.NewObjectID()
+	customer.User.ID = primitive.NewObjectID()
+	err := u.userDB.SaveUser(ctx, customer.User)
+	if err != nil {
+		return dto.RegisterCustomer{}, err
+	}
+
+	payment, err := u.PaymentDB.GetPaymentRule(ctx, bson.M{"_id": customer.PaymentRuleID})
+	if err != nil {
+		u.userDB.DeleteUser(ctx, customer.User.ID)
+		return dto.RegisterCustomer{}, err
+	}
+	if len(payment) == 0 {
+		u.userDB.DeleteUser(ctx, customer.User.ID)
+		return dto.RegisterCustomer{}, fmt.Errorf("payment rule not found")
+	}
+	day := time.Hour * 24 * time.Duration(payment[0].NumberOfDays)
+	endDate := customer.StarDate.Add(day)
+	customer.EndDate = endDate
+	customer.UserID = customer.User.ID
+	customer.CreatedAt = time.Now()
+	if err := u.db.SaveCustomer(ctx, customer); err != nil {
+		u.userDB.DeleteUser(ctx, customer.User.ID)
+		return dto.RegisterCustomer{}, err
+	}
+	return customer, nil
+}
+
+func (u *customer) GetCustomers(ctx context.Context, filter bson.M) ([]dto.RegisterCustomer, error) {
+	id, ok := filter["id"]
+	if !ok {
+		return u.db.GetCustomer(ctx, filter)
+	}
+	hexID, _ := primitive.ObjectIDFromHex(id.(string))
+	fil := bson.M{"_id": hexID}
+	return u.db.GetCustomer(ctx, fil)
+
+}
+
+func (u *customer) UpdateCustomer(ctx context.Context, customerRequest dto.RegisterCustomer) (dto.RegisterCustomer, error) {
+
+	userUpdate := bson.M{}
+	if customerRequest.User.FirstName != "" {
+		userUpdate["first_name"] = customerRequest.User.FirstName
+	}
+	if customerRequest.User.LastName != "" {
+		userUpdate["last_name"] = customerRequest.User.FirstName
+
+	}
+	if customerRequest.User.Phone != "" {
+		userUpdate["phone"] = customerRequest.User.Phone
+	}
+	if err := u.userDB.UpdateUser(ctx, bson.M{"_id": customerRequest.UserID}, userUpdate); err != nil {
+		return dto.RegisterCustomer{}, err
+	}
+
+	if err := u.db.UpdateCustomer(ctx, bson.M{"_id": customerRequest.ID}, customerRequest); err != nil {
+		return dto.RegisterCustomer{}, err
+	}
+	return customerRequest, nil
+}
+
+func (u *customer) DeleteCustomer(ctx context.Context, customerRequest dto.RegisterCustomer) error {
+	return u.db.DeleteCustomer(ctx, customerRequest.ID)
+}
--- internal/storage/persistence/customer/customer.go
+++ internal/storage/persistence/customer/customer.go
@@ -6,6 +6,8 @@ import (
 	"github.com/alazarbeyeneazu/gms-backend/internal/constants/errors"
 	"github.com/alazarbeyeneazu/gms-backend/internal/constants/model/dto"
 	"github.com/alazarbeyeneazu/gms-backend/internal/storage/persistence"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/zap"
 )
@@ -21,11 +23,59 @@ func Init(db *mongo.Collection, log zap.Logger) persistence.Customer {
 		log: log,
 	}
 }
-func (u *customer) SaveCustomer(ctx context.Context, user dto.User) error {
-	_, err := u.db.InsertOne(ctx, user)
+func (u *customer) SaveCustomer(ctx context.Context, customer dto.RegisterCustomer) error {
+	_, err := u.db.InsertOne(ctx, customer)
 	if err != nil {
-		u.log.Error("Save User", zap.Error(err))
-		err = errors.ErrUnableTocreate.Wrap(err, err.Error(), "user", user)
+		u.log.Error("Save customer", zap.Error(err))
+		err = errors.ErrUnableTocreate.Wrap(err, err.Error(), "customer", customer)
+		return err
+	}
+	return nil
+}
+func (u *customer) GetCustomer(ctx context.Context, filter bson.M) ([]dto.RegisterCustomer, error) {
+	var customers []dto.RegisterCustomer
+	result, err := u.db.Find(ctx, filter)
+	if err != nil {
+		u.log.Error("error while getting payment rule", zap.Error(err))
+		err = errors.ErrUnableToUpdate.Wrap(err, err.Error(), zap.Any("filter", filter))
+		return nil, err
+	}
+
+	defer result.Close(ctx)
+
+	for result.Next(ctx) {
+		var customer dto.RegisterCustomer
+		if err := result.Decode(&customer); err != nil {
+			u.log.Error("error decoding payment rule", zap.Error(err))
+			err = errors.ErrUnableToUpdate.Wrap(err, err.Error(), zap.Any("filter", filter))
+			return nil, err
+		}
+		customers = append(customers, customer)
+	}
+
+	if err := result.Err(); err != nil {
+		u.log.Error("error while iterating payment rule result", zap.Error(err))
+		err = errors.ErrUnableToUpdate.Wrap(err, err.Error(), zap.Any("filter", filter))
+		return nil, err
+	}
+
+	return customers, nil
+}
+func (u *customer) UpdateCustomer(ctx context.Context, filter bson.M, update dto.RegisterCustomer) error {
+	_, err := u.db.UpdateOne(ctx, filter, bson.M{"$set": update})
+	if err != nil {
+		u.log.Error("error while updating customer", zap.Error(err))
+		err = errors.ErrUnableToUpdate.Wrap(err, err.Error(), zap.Any("filter", filter), zap.Any("update", update))
+		return err
+	}
+	return nil
+}
+
+func (u *customer) DeleteCustomer(ctx context.Context, customerID primitive.ObjectID) error {
+	_, err := u.db.DeleteOne(ctx, bson.M{"_id": customerID})
+	if err != nil {
+		u.log.Error("error while updating customer", zap.Error(err))
+		err = errors.ErrUnableToUpdate.Wrap(err, err.Error(), zap.Any("userID", customerID))
 		return err
 	}
 	return nil
