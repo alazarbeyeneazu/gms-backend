987e95a417e52a0e7b63fefc6819e6398640418d
alazarbeyeneazu <alazarbeyene99@gmail.com>
2024-04-29 15:59:28 +0300

    login handler done

--- internal/module/auth/auth.go
+++ internal/module/auth/auth.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/alazarbeyeneazu/gms-backend/internal/constants/errors"
 	"github.com/alazarbeyeneazu/gms-backend/internal/constants/model/dto"
@@ -10,6 +11,7 @@ import (
 	"github.com/alazarbeyeneazu/gms-backend/internal/storage/persistence"
 	"github.com/alazarbeyeneazu/gms-backend/platform/utils"
 	"github.com/go-playground/validator"
+	"github.com/golang-jwt/jwt/v4"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.uber.org/zap"
@@ -19,13 +21,15 @@ type userAuth struct {
 	log             *zap.Logger
 	userDB          persistence.User
 	authPersistence persistence.Auth
+	authKey         string
 }
 
-func Init(userdb persistence.User, authdb persistence.Auth, log *zap.Logger) module.Auth {
+func Init(userdb persistence.User, authdb persistence.Auth, authkey string, log *zap.Logger) module.Auth {
 	return &userAuth{
 		log:             log,
 		userDB:          userdb,
 		authPersistence: authdb,
+		authKey:         authkey,
 	}
 }
 func (u *userAuth) RegisterUserAuth(ctx context.Context, userAuth dto.UserAuth) error {
@@ -58,5 +62,40 @@ func (u *userAuth) RegisterUserAuth(ctx context.Context, userAuth dto.UserAuth) 
 		Password: pass,
 	}
 	return u.authPersistence.SaveUserAuth(ctx, authp)
+
+}
+func (u *userAuth) Login(ctx context.Context, loginRequst dto.LoginRequest) (string, error) {
+	validate := validator.New()
+	if err := validate.Struct(&loginRequst); err != nil {
+		u.log.Error("validation error ", zap.Error(err))
+		err = errors.ErrInvalidUserInput.Wrap(err, err.Error())
+		return "", err
+	}
+	user, err := u.userDB.GetUsers(ctx, bson.M{"phone": loginRequst.Phone})
+	if err != nil {
+		return "", err
+	}
+	password, err := u.authPersistence.GetAuth(ctx, bson.M{"user_id": user[0].ID})
+	if err != nil {
+		return "", err
+	}
+	if err := utils.ComparePasswords(password.Password, loginRequst.Password); err != nil {
+		return "", err
+	}
+	expirationTime := time.Now().Add(time.Hour * 16)
+	claims := &dto.Claim{
+		UserID: password.UserID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(u.authKey))
+	if err != nil {
+		u.log.Error("validation error ", zap.Error(err))
+		err = errors.ErrUnExpectedError.Wrap(err, err.Error())
+		return "", err
+	}
+	return tokenString, nil
 
 }
